f569c4d481a8a90d9c167de7ac89f42e66ef3878
Alex P <Alex.peschel@gmail.com>
2025-08-19 11:35:13 +0200

    first commit

---
+++ openAICheck/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+//how to run this file:
+// 1. go run main.go
+// 2. then open http://localhost:8080/docs/ in your browser
+// 3. This file serves the OpenAPI YAML file and Swagger UI for the Recipe Manager API
+
+func main() {
+	// Serve OpenAPI YAML file
+	// The path to the OpenAPI YAML file is relative to the main.go file
+	// If issues occur, it can bve with swagger-initializer.js in the swagger-ui/dist folder
+	http.HandleFunc("/docs/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")             // prevent browser caching
+		http.ServeFile(w, r, "../openapis/openapiExample.yaml") // path relative to main.go
+	})
+
+	// Serve Swagger UI static files
+	fs := http.FileServer(http.Dir("../swagger-ui/dist")) // relative to main.go
+	http.Handle("/docs/", http.StripPrefix("/docs/", fs))
+
+	log.Println("Server running at http://localhost:8080/docs/")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

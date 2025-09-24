3b1249f1c052c07b5f70629b4b3c125067cc5d2f
Alex P <Alex.peschel@gmail.com>
2025-09-24 12:00:03 +0200

    [Dockerize] create a docker setup for the swagger-ui to run on the side of the APIs

--- openAICheck/main.go
+++ openAICheck/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 //how to run this file:
@@ -10,27 +11,41 @@ import (
 // 2. then open http://localhost:8080/docs/ in your browser
 // 3. This file serves the OpenAPI YAML file and Swagger UI for the Recipe Manager API
 
+func runningInDocker() bool {
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		return true
+	}
+	return false
+}
+
 func main() {
 	// Serve OpenAPI YAML file
 	// The path to the OpenAPI YAML file is relative to the main.go file
 	// If issues occur, it can bve with swagger-initializer.js in the swagger-ui/dist folder
 	http.HandleFunc("/docs/combined.yaml", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-store")       // prevent browser caching
-		http.ServeFile(w, r, "../openapis/combined.yaml") // path relative to main.go
+		w.Header().Set("Cache-Control", "no-store")      // prevent browser caching
+		http.ServeFile(w, r, "./openapis/combined.yaml") // path relative to main.go
 	})
+
+	var startPath string
+	if runningInDocker() {
+		startPath = "./"
+	} else {
+		startPath = "../"
+	}
 
 	http.HandleFunc("/docs/recipe_api.yaml", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-store")
-		http.ServeFile(w, r, "../smallProjects/recipe_manager_api/openapi.yaml")
+		http.ServeFile(w, r, startPath+"smallProjects/recipe_manager_api/openapi.yaml")
 	})
 
 	http.HandleFunc("/docs/task_api.yaml", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-store")
-		http.ServeFile(w, r, "../smallProjects/task_tracker_api/openapi.yaml")
+		http.ServeFile(w, r, startPath+"smallProjects/task_tracker_api/openapi.yaml")
 	})
 
 	// Serve Swagger UI static files
-	fs := http.FileServer(http.Dir("../swagger-ui/dist")) // relative to main.go
+	fs := http.FileServer(http.Dir(startPath + "swagger-ui/dist")) // relative to main.go
 	http.Handle("/docs/", http.StripPrefix("/docs/", fs))
 
 	log.Println("Server running at http://localhost:8080/docs/")
